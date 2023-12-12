82e4297c7182ee9203d221d49474a84bcd860977
Nick Kelly <k05nk01@gmail.com>
2023-12-12 11:19:39 -0800

    Added sunrise/ sunset data from api

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 import requests
+from datetime import datetime as dt
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+SEATTLE_LATITUDE = 47.6061
+SEATTLE_LONGITUDE = -122.3328
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (latitude, longitude)
+#
+# print(iss_position)
+
+# ------------------ check if it's dark out ---------------
+
+parameters = {
+    "lat": SEATTLE_LATITUDE,
+    "lng": SEATTLE_LONGITUDE,
+    "formatted": 0,
+    "tzId": "America/Los_Angeles",
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 
 data = response.json()
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
 
-iss_position = (latitude, longitude)
+sunrise_time = sunrise.split("T")[1].split("-")[0]
+sunset_time = sunset.split("T")[1].split("-")[0]
 
-print(iss_position)
+print(sunrise)
+print(sunrise_time)
+
+time_now = str(dt.now()).split(" ")[1].split(".")[0]
+
+print(time_now)
