23ceeb2aeda6b5072bcabf0d75c037a1b0dbe042
Nick Kelly <k05nk01@gmail.com>
2023-12-12 21:39:23 -0800

    Datetimes complete, comparison features complete. Need to set up SMTP or SMS.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import datetime as dt
 SEATTLE_LATITUDE = 47.6061
 SEATTLE_LONGITUDE = -122.3328
 
+# ---------------- Where is the ISS? --------------#
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
@@ -14,7 +15,7 @@ iss_latitude = float(data["iss_position"]["latitude"])
 
 iss_position = (iss_latitude, iss_longitude)
 
-print(iss_position)
+# print(iss_position)
 
 # ------------------ check if it's dark out ---------------
 
@@ -36,19 +37,32 @@ sunset = data["results"]["sunset"]
 # sunrise_time = sunrise.split("T")[1].split("-")[0]
 # sunset_time = sunset.split("T")[1].split("-")[0]
 
+# convert strings into datetime objects so you can compare them to current time like civilized machines
 sunrise_time = dt.datetime.strptime(sunrise, '%Y-%m-%dT%H:%M:%S%z').time()
 sunset_time = dt.datetime.strptime(sunset, '%Y-%m-%dT%H:%M:%S%z').time()
 
-print(sunrise)
-print(sunrise_time)
+# print(sunrise)
+# print(sunrise_time)
 
 # time_now = str(dt.datetime.now()).split(" ")[1].split(".")[0]
-print(time_now)
 
-#If the ISS is close to my current position
+# --------------- what time is it right now? ------------
+time_now = dt.datetime.now().time()
+# print(time_now)
+# print(time_now > sunset_time)
 
-# if abs(iss_latitude - SEATTLE_LATITUDE) <= 5 and
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+# is the ISS within 5 degrees of me?
+
+iss_within_5deg_lat = (abs(iss_latitude - SEATTLE_LATITUDE) <= 5)
+iss_within_5deg_lng = (abs(iss_longitude - SEATTLE_LONGITUDE) <= 5)
+
+# is it dark out?
+
+is_day = (sunrise_time <= time_now <= sunset_time)
+
+
+if iss_within_5deg_lat and iss_within_5deg_lng and not is_day:
+    # this is where I'd email myself or SMS
+    pass
+
 
